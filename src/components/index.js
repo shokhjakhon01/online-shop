63f79de353c2001d34af7393709e66964e0671d5
muhammad1749 <muhammadqodirmuminov2@gmail>
2023-01-30 14:57:29 +0500

    jsz

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./novbar";
 export { default as Valodation } from "./valodation";
 export { default as Main } from "./main";
+export { default as Footer } from "./footer";
 
