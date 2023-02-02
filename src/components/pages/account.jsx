6ab8a87023a7a1687d5340b953d15851120eb09f
shohjahon1code <suyunovshohjahon08@gmail.com>
2023-02-02 14:38:33 +0500

    finish

--- src/components/pages/account.jsx
+++ src/components/pages/account.jsx
@@ -6,60 +6,66 @@ import styles from "../styles/account.module.css";
 import { dataAccountBtn } from "../../service/data";
 import { useNavigate } from "react-router-dom";
 import ButtonAdmin from "../button-admin";
+import Loader from "../loader";
 
 const Account = () => {
-	const [state, setState] = useState([
-		{
-			user: {
-				username: "",
-				bio: "lorem10",
-				image: "https://icons.veryicon.com/png/o/application/cloud-supervision-platform-vr10/admin-5.png",
-				email: "lorem10@gmail.com",
-			},
-		},
-	]);
-	const [value, setValue] = useState(1);
-	const navigate = useNavigate();
+  const [loader, setLoader] = useState(false);
+  const [state, setState] = useState([
+    {
+      user: {
+        username: "",
+        bio: "lorem10",
+        image:
+          "https://icons.veryicon.com/png/o/application/cloud-supervision-platform-vr10/admin-5.png",
+        email: "lorem10@gmail.com",
+      },
+    },
+  ]);
+  const [value, setValue] = useState(1);
+  const navigate = useNavigate();
 
-	const btnHandler = (e) => {
-		setValue(e.target.dataset.id);
+  const btnHandler = (e) => {
+    setValue(e.target.dataset.id);
 
-		if (+value === 2) {
-			navigate("/account/posts");
-		}
-	};
+    if (+value === 2) {
+      navigate("/account/posts");
+    }
+  };
 
-	const fetchUsers = async () => {
-		const token = localStorage.getItem("token");
+  const fetchUsers = async () => {
+    setLoader(true);
+    const token = localStorage.getItem("token");
 
-		const response = await fetch("https://api.realworld.io/api/user", {
-			headers: { Authorization: `Token ${token}` },
-		});
-		const user = await response.json();
+    const response = await fetch("https://api.realworld.io/api/user", {
+      headers: { Authorization: `Token ${token}` },
+    });
+    const user = await response.json();
 
-		setState([user]);
-	};
+    setState([user]);
+    setLoader(false);
+  };
 
-	useEffect(() => {
-		fetchUsers();
-	}, []);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-	return (
-		<div className="container">
-			<div className={styles.admin}>
-				<ButtonAdmin
-					dataAccountBtn={dataAccountBtn}
-					btnHandler={btnHandler}
-				/>
+  if (loader) {
+    return <Loader />;
+  }
 
-				<AdminUser state={state} />
+  return (
+    <div className="container">
+      <div className={styles.admin}>
+        <ButtonAdmin dataAccountBtn={dataAccountBtn} btnHandler={btnHandler} />
 
-				<AdminActive />
+        <AdminUser state={state} />
 
-				<AdminPay />
-			</div>
-		</div>
-	);
+        <AdminActive />
+
+        <AdminPay />
+      </div>
+    </div>
+  );
 };
 
 export default Account;
