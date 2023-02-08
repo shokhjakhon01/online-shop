fc35fc0afafae2b85ff1426fd78fbde024159a2e
muhammad1749 <muhammadqodirmuminov2@gmail>
2023-02-08 16:31:42 +0500

    404page added

--- src/pages/notFount.jsx
+++ src/pages/notFount.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "../styles/404page.module.css";
 
 const NotFount = () => {
-	return <div>404 page</div>;
+	return (
+		<div className={styles.body}>
+			<h1 className={styles.title}>404</h1>
+			<div className={styles["cloak__wrapper"]}>
+				<div className={styles["cloak__container"]}>
+					<div className={styles.cloak}></div>
+				</div>
+			</div>
+			<div className={styles.info}>
+				<h2>We can't find that page</h2>
+				<p className={styles.subtitle}>
+					We're fairly sure that page used to be here, but seems to have
+					gone missing. We do apologise on it's behalf.
+				</p>
+				<a
+					className={styles.a}
+					href="/"
+					target="_blank"
+					rel="noreferrer noopener">
+					Home
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default NotFount;
