b9f08562f0140b65cbcfc9439bdd3b0fcf5e6f9f
shohjahon1code <suyunovshohjahon08@gmail.com>
2023-02-13 19:02:30 +0500

    added-pagination

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,35 +1,31 @@
+
 import AllProducts from "./allProducts";
 import Categories from "./categories";
+import Pagination from "./Pagination";
 import Slider from "./slider";
 
-const Main = () => {
-	return (
-		<div className="container">
-			<div className="main__hero">
-				<Categories />
-				<Slider />
-			</div>
-			<div className="row row-2">
-				<h2>All Products</h2>
-				<select>
-					<option value="">Default Shorting</option>
-					<option value="">Short by price</option>
-					<option value="">Short by popularity</option>
-					<option value="">Short by rating</option>
-					<option value="">Short by sale</option>
-				</select>
-			</div>
-			<AllProducts />
 
-			<div className="page-btn">
-				<span>1</span>
-				<span>2</span>
-				<span>3</span>
-				<span>4</span>
-				<span>&#8594;</span>
-			</div>
-		</div>
-	);
+const Main = () => {
+	
+  return (
+    <div className="container">
+      <div className="main__hero">
+        <Categories />
+        <Slider />
+      </div>
+      <div className="row row-2">
+        <h2>All Products</h2>
+        <select>
+          <option value="">Default Shorting</option>
+          <option value="">Short by price</option>
+          <option value="">Short by popularity</option>
+          <option value="">Short by rating</option>
+          <option value="">Short by sale</option>
+        </select>
+      </div>
+      <AllProducts />
+    </div>
+  );
 };
 
 export default Main;
